8f428bb4f1e41bb2cca2d9233e6673c1e1763d4c
Darshan Shivashankar <darshan1185@gmail.com>
2019-02-10 16:32:56 -0800

    Create bundle.sh

---
+++ bundle.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set +v
+set -e
+
+autopath=`pwd`
+
+workspace=$1
+projectName=$2
+type=$3
+sharedflowPomPath=$1/src/sharedflows/$2
+proxyPomPath=$1/src/gateway/$2
+
+buildSharedFlow(){
+	cd $autopath
+	echo "Maven Clean:"
+	mvn clean -X -f $sharedflowPomPath/pom.xml
+
+	cd $autopath
+	echo "Bundling SharedFlow:" $projectName
+	mvn package -X -f $sharedflowPomPath/pom.xml -Ptest
+}
+
+buildProxy(){
+	cd $autopath
+	echo "Maven Clean:"
+	mvn clean -X -f $sharedflowPomPath/pom.xml
+
+	cd $autopath
+	echo "Bundling Proxy:" $projectName
+	mvn package -X -f $proxyPomPath/pom.xml -Ptest
+}
+
+main(){
+	echo "Type is missing in the project.yaml"
+}
+
+if [[ $type == "sharedflow" ]]
+	then
+		buildSharedFlow
+elif [[ $type == "proxy" ]]
+		then
+			buildProxy
+else
+	main
